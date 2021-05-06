85699b6e3548323866546c133dc9497487aba7a2
Aleksandr <alex@enslit.ru>
2021-05-06 23:01:53 +0300

    build: Реализовать модуль auth api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,14 @@
-class Api {
+class FetchApi {
   constructor(options) {
     this._baseUrl = options.baseUrl;
     this._headers = options.headers;
   }
 
-  _fetch(url, method = 'GET', body = null) {
+  _fetch(url, method = 'GET', body = null, additionalHeaders = {}) {
     const fetchUrl = `${this._baseUrl}/${url}`;
     const options = {
       method,
-      headers: this._headers,
+      headers: Object.assign(this._headers, additionalHeaders),
     };
 
     if (body) {
@@ -25,7 +25,27 @@ class Api {
       return res.json();
     });
   }
+}
 
+class AuthApi extends FetchApi {
+  register(email, password) {
+    return this._fetch('/signup', 'POST', { email, password });
+  }
+
+  auth(email, password) {
+    return this._fetch('/signin', 'POST', { email, password });
+  }
+
+  checkToken(token) {
+    const additionalHeaders = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    return this._fetch('/users/me', 'GET', null, additionalHeaders);
+  }
+}
+
+class AppApi extends FetchApi {
   getUserInfo() {
     return this._fetch('users/me');
   }
@@ -55,10 +75,17 @@ class Api {
   }
 }
 
-export const api = new Api({
+export const api = new AppApi({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-21',
   headers: {
     authorization: '52186c90-0ae5-45bb-99b5-e4acaa2b939f',
+    'Content-Type': 'application/json',
+  },
+});
+
+export const auth = new AuthApi({
+  baseUrl: 'https://auth.nomoreparties.co/',
+  headers: {
     'Content-Type': 'application/json',
   },
 });
