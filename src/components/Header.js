13b0b6a3c422921168ff707cb393fad2e5240ea1
Aleksandr <alex@enslit.ru>
2021-05-06 22:00:25 +0300

    build: Реализовать меню

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,48 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useLocation, useHistory } from 'react-router-dom';
-import logo from '../images/logo.svg';
 import CurrentUserContext from '../contexts/CurrentUserContext';
-import { object, string } from 'prop-types';
+import { bool, func, object, string } from 'prop-types';
+import logo from '../assets/images/logo.svg';
+import menuIcon from '../assets/icons/menu.svg';
+import closeIcon from '../assets/icons/close_icon.svg';
 
 UserInfo.propTypes = {
   user: object,
+  openMenu: bool,
+  setOpenMenu: func,
 };
 
 HeaderLink.propTypes = {
   path: string,
 };
 
-function UserInfo({ user }) {
+function UserInfo({ user, openMenu, setOpenMenu }) {
   const history = useHistory();
   const { setIsloggedIn } = useContext(CurrentUserContext);
 
   const logout = () => {
     localStorage.removeItem('jwt');
+    setOpenMenu(false);
     setIsloggedIn(false);
     history.push('/sign-in');
   };
 
   return (
-    <div>
-      <span>{user.name}</span>
-      <button onClick={logout} className="link header__link">
-        Выйти
-      </button>
-    </div>
+    <>
+      <div className={`header__menu ${openMenu ? 'header__menu_visible' : ''}`}>
+        <span className="header__email">{user.name}</span>
+        <button onClick={logout} className="link header__link">
+          Выйти
+        </button>
+      </div>
+      <button
+        onClick={() => setOpenMenu(!openMenu)}
+        type="button"
+        aria-label="Меню"
+        className="btn btn_type_menu"
+        style={{ backgroundImage: `url(${openMenu ? closeIcon : menuIcon}` }}
+      />
+    </>
   );
 }
 
@@ -46,16 +60,23 @@ function HeaderLink({ path }) {
 }
 
 function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
   const { isLoggedIn, currentUser } = useContext(CurrentUserContext);
   const { pathname } = useLocation();
 
   return (
-    <header className="header container">
+    <header
+      className={`header container ${openMenu ? 'header_menu-opened' : ''}`}
+    >
       <a href="/" className="logo" target="_self">
         <img src={logo} alt="Mesto Russia" className="logo__img" />
       </a>
       {isLoggedIn ? (
-        <UserInfo user={currentUser} />
+        <UserInfo
+          user={currentUser}
+          openMenu={openMenu}
+          setOpenMenu={setOpenMenu}
+        />
       ) : (
         <HeaderLink path={pathname} />
       )}
